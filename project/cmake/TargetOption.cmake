43c4be79156578f9400ae67c573092cb6bb4b1fa
owt5008137 <owt5008137@live.com>
2017-10-01 14:08:51 +0800

    使用统一的构建流程 使用ctest驱动单元测试

--- project/cmake/TargetOption.cmake
+++ project/cmake/TargetOption.cmake
@@ -1,17 +1,21 @@
+﻿# 设置实际的默认编译输出目录，为防止呗外部模块设置，所以要先判定一下
 
-# =========== 环境检查 ===========
-if (NOT　DEFINED　ARCH_SUFFIX)
-    # x86 or x86_64
-    if(CMAKE_SIZEOF_VOID_P MATCHES 8)
-        set (ARCH_SUFFIX "64")
-    else()
-        set (ARCH_SUFFIX "")
-    endif()
-
-    # 设置实际的默认编译输出目录
-    set(EXECUTABLE_OUTPUT_PATH "${PROJECT_BINARY_DIR}/bin")
-    set(LIBRARY_OUTPUT_PATH "${PROJECT_BINARY_DIR}/lib${ARCH_SUFFIX}")
-
-    link_directories("${PROJECT_SOURCE_DIR}/lib${ARCH_SUFFIX}")
-
+if (NOT CMAKE_ARCHIVE_OUTPUT_DIRECTORY)
+    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/lib")
 endif()
+
+if (NOT CMAKE_LIBRARY_OUTPUT_DIRECTORY)
+    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/lib")
+endif()
+
+if (NOT CMAKE_RUNTIME_OUTPUT_DIRECTORY)
+    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/bin")
+endif()
+
+set(PROJECT_TARGET_INSTALL_EXECUTABLE_DIR "bin")
+set(PROJECT_TARGET_INSTALL_ARCHIVE_DIR "lib")
+set(PROJECT_TARGET_INSTALL_LIBRARY_DIR "lib")
+
+# set(EXECUTABLE_OUTPUT_PATH "${PROJECT_BINARY_DIR}/bin")
+
+link_directories (${CMAKE_ARCHIVE_OUTPUT_DIRECTORY})
