0c2788b28c07abc5bbc2d0972979c68e0407716d
owent <admin@owent.net>
2026-05-26 20:10:36 +0800

    Fixes dll export

--- include/detail/crc16.h
+++ include/detail/crc16.h
@@ -7,12 +7,14 @@
 
 #pragma once
 
-#include <stddef.h>
-#include <stdint.h>
+#include <cstddef>
+#include <cstdint>
+
+#include <detail/hiredis_happ_config.h>
 
 namespace hiredis {
 namespace happ {
-uint16_t crc16(const char *buf, size_t len);
+HIREDIS_HAPP_API uint16_t crc16(const char *buf, size_t len);
 }
 }  // namespace hiredis
 
